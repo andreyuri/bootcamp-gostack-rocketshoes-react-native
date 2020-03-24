ccb166db65184a9479725c1f9c71107d379c9cf0
André Yuri <andre_yuri@live.com>
2020-03-24 19:00:31 -0300

    listagem de produtos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,72 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { IntlProvider, FormattedNumber } from 'react-intl';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Main() {
-  return <Container />;
+import {
+  Container,
+  Product,
+  Image,
+  Title,
+  Price,
+  AddButtom,
+  AddButtomText,
+  AddButtomProductAmount,
+  AddButtomProductAmountText,
+} from './styles';
+
+import api from '../../services/api';
+
+class Main extends Component {
+  static navigationOptions = {
+    title: 'Rocketshoes',
+  };
+
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get(`/products`);
+
+    this.setState({ products: response.data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <IntlProvider locale="pt-BR">
+        <Container>
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={products}
+            keyExtractor={(product) => String(product.id)}
+            renderItem={({ item }) => (
+              <Product>
+                <Image source={{ uri: item.image }} />
+                <Title>{item.title}</Title>
+                <Price>
+                  <FormattedNumber
+                    value={item.price}
+                    style="currency"
+                    currency="BRL"
+                  />
+                </Price>
+                <AddButtom>
+                  <AddButtomProductAmount>
+                    <Icon name="add-shopping-cart" size={20} color="#FFF" />
+                    <AddButtomProductAmountText>0</AddButtomProductAmountText>
+                  </AddButtomProductAmount>
+                  <AddButtomText>ADICIONAR</AddButtomText>
+                </AddButtom>
+              </Product>
+            )}
+          />
+        </Container>
+      </IntlProvider>
+    );
+  }
 }
 
-Main.navigationOptions = {
-  title: 'Rocketshoes',
-};
+export default Main;
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,61 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import { darken } from 'polished';
 
 export const Container = styled.View`
   flex: 1;
-  padding: 30px;
+  background: #191920;
+`;
+
+export const Product = styled.View`
+  background: #fff;
+  padding: 10px;
+  margin: 15px;
+  border-radius: 4px;
+  width: 220px;
+  height: 50%;
+`;
+
+export const Image = styled.Image`
+  height: 200px;
+  width: 200px;
+`;
+
+export const Title = styled.Text.attrs({ numberOfLines: 1 })`
+  color: #333333;
+  font-size: 16px;
+`;
+
+export const Price = styled.Text`
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const AddButtom = styled(RectButton)`
+  background: #7159c1;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 4px;
+  margin: 10px 0;
+`;
+
+export const AddButtomText = styled.Text`
+  color: #fff;
+  flex: 1;
+  text-align: center;
+  font-weight: bold;
+`;
+
+export const AddButtomProductAmount = styled.View`
+  padding: 10px 20px;
+  flex-direction: row;
+  align-items: center;
+  background: ${darken(0.03, '#7159c1')};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const AddButtomProductAmountText = styled.Text`
+  color: #fff;
+  margin-left: 5px;
 `;
